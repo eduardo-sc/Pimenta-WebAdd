8a0db296f57c00cba6440e4eb58b8c66abb954a1
Eduardo santana da cruz <eduardo.santana95@hotmail.com>
2022-08-30 21:38:40 -0300

    tela de listar Produto

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,20 +1,34 @@
+import Head from "next/head";
 import { useState } from "react";
+import { FiRefreshCcw } from "react-icons/fi";
 import ReactModal from "react-modal";
 import { Header } from "../../componets/Header";
 import ModalProduct from "../../componets/ModalProduct";
 import { setupAPICliet } from "../../services/api";
 import { canSSRAuth } from "../../Utils/canSSRAuth";
-
+import styles from "./styles.module.scss";
 type ItensProps = {
   id: string;
   name: string;
 };
 interface CategoryListProps {
   CategoryProps: ItensProps[];
+  Produtos: ItensProdProps[];
 }
-export default function Products({ categoryList }: CategoryListProps | any) {
+type ItensProdProps = {
+  id: string;
+  name: string;
+  price: string;
+  description: string;
+  banner: string;
+};
+export default function Products({
+  categoryList,
+  products,
+}: CategoryListProps | any) {
   const [modalVisible, setModalVisible] = useState(false);
   const [categories, setCategories] = useState(categoryList || []);
+  const [productsItens, setProductsItens] = useState(products || []);
 
   function abrirModal() {
     setModalVisible(true);
@@ -25,8 +39,38 @@ export default function Products({ categoryList }: CategoryListProps | any) {
   ReactModal.setAppElement("#__next");
   return (
     <>
-      <Header />
       <button onClick={abrirModal}>novoProduto</button>
+
+      <Head>
+        <title>Pedidos Pimenta-Malagueta</title>
+      </Head>
+      <div>
+        <Header />
+
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Ultimos Pedidos</h1>
+
+            <button className={styles.buttonAtualizar}>
+              <FiRefreshCcw size={25} color={"#3fffa3"} />
+            </button>
+            <button className={styles.buttonNovo} onClick={abrirModal}>
+              Novo
+            </button>
+          </div>
+          {productsItens &&
+            productsItens.map((iten: ItensProdProps) => (
+              <article className={styles.listOrder}>
+                <section className={styles.orderItem}>
+                  <button>
+                    <div className={styles.tag}></div>
+                    <span>{iten.name}</span>
+                  </button>
+                </section>
+              </article>
+            ))}
+        </main>
+      </div>
 
       {modalVisible && (
         <ModalProduct
@@ -42,10 +86,12 @@ export const getServerSideProps = canSSRAuth(async (ctx) => {
   const apliClient = setupAPICliet(ctx);
 
   const response = await apliClient.get("/categorys/list");
+  const responseProducts = await apliClient.get("/category/products");
 
   return {
     props: {
       categoryList: response.data,
+      products: responseProducts.data,
     },
   };
 });
